da0b294d0e15bf4dcd606c9d67938cd0579add48
jotasic <embedded61@gamil.com>
2021-03-09 22:19:54 +0900

    CBV로 블로그 포스트 상세 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:pk>/', views.single_post_page),
-    path('', views.index),
+    # CBV 방식으로 구현
+    path('', views.PostList.as_view()),
+    path('<int:pk>/', views.PostDetail.as_view()),
+    # FBV 방식으로 구현
+    # path('', views.index),
+    #path('<int:pk>/', views.single_post_page),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,27 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 
 
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
+# CBV 방식으로 구현
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
 
-    return render(request, 'blog/index.html', {'posts': posts, })
+
+class PostDetail(DetailView):
+    model = Post
+
+# FBV 방식으로 구현
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk')
+
+#     return render(request, 'blog/index.html', {'posts': posts, })
 
 
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
+# def single_post_page(request, pk):
+#     post = Post.objects.get(pk=pk)
 
-    return render(request, 'blog/single_post_page.html', {'post': post, })
+#     return render(request, 'blog/single_post_page.html', {'post': post, })

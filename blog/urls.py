8e13ba440d3cad4876c901693fc222b357f5fe31
jotasic <embedded61@gamil.com>
2021-03-11 15:17:01 +0900

    테그 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     # CBV 방식으로 구현
     path('category/<str:slug>/', views.category_page),
+   path('tag/<str:slug>/', views.tag_page),
     path('', views.PostList.as_view()),
     path('<int:pk>/', views.PostDetail.as_view()),
     # FBV 방식으로 구현

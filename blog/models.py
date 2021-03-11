2bb7262c1e6c28eba58c5c86f30f83458c7da174
jotasic <embedded61@gamil.com>
2021-03-11 11:04:59 +0900

    포스트 목록 페이지의 카테고리 기능 추가

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,15 @@ import os
 
 # Create your models here.
 
+class Category(models.Model) :
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self) :
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
@@ -21,6 +30,8 @@ class Post(models.Model):
 
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
+    category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL, blank=True)
+
     def __str__(self):
         return f"[{self.pk}] {self.title} :: {self.author}"
     # author : 추후 작성
@@ -33,3 +44,4 @@ class Post(models.Model):
 
     def get_file_ext(self):
         return self.get_file_name().split('.')[-1]
+
--- blog/tests.py
+++ blog/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
-from .models import Post
+from .models import Post, Category
 from django.contrib.auth.models import User
 # Create your tests here.
 
@@ -9,62 +9,78 @@ class TestView(TestCase):
 
     def setUp(self):
         self.client = Client()
-        self.user_trump = User.objects.create(username="trump", password="something")
-        self.user_ohbama = User.objects.create(username="ohbama", password="something")
-        
+        self.user_trump = User.objects.create(
+            username="trump", password="something")
+        self.user_ohbama = User.objects.create(
+            username="ohbama", password="something")
+
+        self.category_programming = Category.objects.create(
+            name='programming', slug='programming')
+        self.category_music = Category.objects.create(
+            name='music', slug='music')
+
+        self.post_001 = Post.objects.create(
+            title='첫 번째 포스트 입니다.', content='Hello Django', author=self.user_trump, category=self.category_programming)
+
+        self.post_002 = Post.objects.create(
+            title='두 번째 포스트 입니다.', content='Good Bye Django', author=self.user_ohbama, category=self.category_music)
+
+        self.post_003 = Post.objects.create(
+            title='세 번째 포스트 입니다.', content='Good morning Django', author=self.user_ohbama)
+
+    def category_card_test(self, soup):
+        categories_card = soup.find('div', id='categories-card')
+        self.assertIn('Categories', categories_card.text)
+        self.assertIn(
+            f'{self.category_programming.name} ({self.category_programming.post_set.count()})', categories_card.text)
+        self.assertIn(
+            f'{self.category_music.name} ({self.category_music.post_set.count()})', categories_card.text)
+        self.assertIn(f'미분류 (1)', categories_card.text)
 
     def test_post_list(self):
-        # 1.1 포스트 목록 페이지를 가져온다.
-        reponse = self.client.get('/blog/')
-        # 1.2 정상적으로 페이지가 로드된다.
-        self.assertEqual(reponse.status_code, 200)
-        # 1.3 페이지 타이틀은 Blog 이다.
-        soup = BeautifulSoup(reponse.content, 'html.parser')
-        self.assertEqual(soup.title.text, 'Blog')
-        # 1.4 내비게이션 바가 있다.
-        # 1.5 Blog, About Me라는 문구가 내비게이션 바에 있다.
-        self.navbar_test(soup)
 
-        # 2.1 포스트가 하나도 없다면
+        self.assertEqual(Post.objects.count(), 3)
+
+        reponse = self.client.get('/blog/')
+        self.assertEqual(reponse.status_code, 200)
+        soup = BeautifulSoup(reponse.content, 'html.parser')
+        
+        self.navbar_test(soup)
+        self.category_card_test(soup)
+
+        main_area = soup.find('div', id='main-area')
+        self.assertNotIn('아직 게시물이 없습니다', main_area.text)
+
+        post_001_card = main_area.find('div', id='post-1')
+        self.assertIn(self.post_001.title, post_001_card.text)
+        self.assertIn(self.post_001.category.name, post_001_card.text)
+
+        post_002_card = main_area.find('div', id='post-2')
+        self.assertIn(self.post_002.title, post_002_card.text)
+        self.assertIn(self.post_002.category.name, post_002_card.text)
+
+        post_003_card = main_area.find('div', id='post-3')
+        self.assertIn(self.post_003.title, post_003_card.text)
+        self.assertIn('미분류', post_003_card.text)
+
+        self.assertIn(self.user_trump.username.upper(), main_area.text)
+        self.assertIn(self.user_ohbama.username.upper(), main_area.text)
+
+        Post.objects.all().delete()
         self.assertEqual(Post.objects.count(), 0)
-        # 2.2 main area에 아직 게시물이 없습니다 라는 문구가 나타난다.
+        reponse = self.client.get('/blog/')
+        soup = BeautifulSoup(reponse.content, 'html.parser')
         main_area = soup.find('div', id='main-area')
         self.assertIn('아직 게시물이 없습니다', main_area.text)
 
-        # 3.1 포스트가 2개 있다면
-        post_001 = Post.objects.create(
-            title='첫 번째 포스트 입니다.', content='Hello Django', author=self.user_trump)
-
-        post_002 = Post.objects.create(
-            title='두 번째 포스트 입니다.', content='Good Bye Django', author=self.user_ohbama)
-
-        self.assertEqual(Post.objects.count(), 2)
-
-        # 3.2 포스트 목록 페이지를 새로고침했을 때
-        reponse = self.client.get('/blog/')
-        soup = BeautifulSoup(reponse.content, 'html.parser')
-        self.assertEqual(reponse.status_code, 200)
-
-        # 3.3 main area에 포스트 2개의 제목이 존재한다.
-        main_area = soup.find('div', id='main-area')
-
-        self.assertIn(post_001.title, main_area.text)
-        self.assertIn(post_002.title, main_area.text)
-        # 3.4 아직 게시물이 없습니다 라는 문구는 더 이상 나타나지 않는다.
-        self.assertNotIn('아직 게시물이 없습니다', main_area.text)
-
-        # 3.5 
-        self.assertIn(self.user_trump.username.upper(), main_area.text)
-        self.assertIn(self.user_ohbama.username.upper(), main_area.text)
-        
-
     def test_post_detail(self):
-        # 1.1 포스트가 하나 있다.
-        post_001 = Post.objects.create(title='첫번째 포스트', content='첫번째 포스트 입니다', author=self.user_trump)
+        # 1.1 포스트가 하나 있다. => setup 포스트에서 생성하도록 변경
+        # post_001 = Post.objects.create(
+        #     title='첫번째 포스트', content='첫번째 포스트 입니다', author=self.user_trump)
         # 1.2 그 포스트의 url은 /blog/1/ 이다.
-        self.assertEqual(post_001.get_absolute_url(), '/blog/1/')
+        self.assertEqual(self.post_001.get_absolute_url(), '/blog/1/')
         # 2.1 첫번째 포스트의 url로 접근하면 정상적으로 작동한다(status code : 200)
-        response = self.client.get(post_001.get_absolute_url())
+        response = self.client.get(self.post_001.get_absolute_url())
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, 'html.parser')
 
@@ -72,18 +88,18 @@ class TestView(TestCase):
         self.navbar_test(soup)
 
         # 2.3 첫 번째 포스트의 제목이 웹 브라우저 타이틀에 있다.
-        self.assertIn(post_001.title, soup.title.text)
+        self.assertIn(self.post_001.title, soup.title.text)
 
         # 2.4 첫 번째 포스트의 제목이 포스트 영역에 있다.
         main_area = soup.find('div', id='main-area')
         post_area = main_area.find('div', id='post-area')
-        self.assertIn(post_001.title, post_area.text)
+        self.assertIn(self.post_001.title, post_area.text)
 
         # 2.5 첫 번째 포스트의 작성자가 포스트 영역에 있다
         # 아직 작성 불가
 
         # 2.6 첫 번째 포스트 내용이 포스트 영역 안에 있다.
-        self.assertIn(post_001.content, post_area.text)
+        self.assertIn(self.post_001.content, post_area.text)
 
         self.assertIn(self.user_trump.username.upper(), post_area.text)
 

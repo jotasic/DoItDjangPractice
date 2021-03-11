ae057bc1d80aa2be65bf6d5990d95c88d3d05c0a
jotasic <embedded61@gamil.com>
2021-03-11 21:38:54 +0900

    포스트 수정할 때 텍스트로 태그 지정하기

--- blog/views.py
+++ blog/views.py
@@ -15,59 +15,64 @@ class PostList(ListView):
     model = Post
     ordering = '-pk'
 
-    def get_context_data(self, **kwargs) :
+    def get_context_data(self, **kwargs):
         context = super(PostList, self).get_context_data()
         context['categories'] = Category.objects.all()
-        context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        context['no_category_post_count'] = Post.objects.filter(
+            category=None).count()
         return context
+
 
 class PostDetail(DetailView):
     model = Post
 
-    def get_context_data(self, **kwargs) :
+    def get_context_data(self, **kwargs):
         context = super(PostDetail, self).get_context_data()
         context['categories'] = Category.objects.all()
-        context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        context['no_category_post_count'] = Post.objects.filter(
+            category=None).count()
         return context
 
 
 class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = Post
-    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
+    fields = ['title', 'hook_text', 'content',
+        'head_image', 'file_upload', 'category']
 
-    def test_func(self) :
+    def test_func(self):
         return self.request.user.is_superuser or self.request.user.is_staff
 
-    def form_valid(self, form) :
+    def form_valid(self, form):
         current_user = self.request.user
-        if current_user.is_authenticated and (current_user.is_staff or current_user.is_superuser) :
+        if current_user.is_authenticated and (current_user.is_staff or current_user.is_superuser):
             form.instance.author = current_user
             response = super(PostCreate, self).form_valid(form)
 
             tags_str = self.request.POST.get('tags_str')
 
-            if tags_str :
+            if tags_str:
                 tags_str = tags_str.strip()
 
                 tags_str = tags_str.replace(',', ';')
                 tags_list = tags_str.split(';')
 
-                for t in tags_list :
+                for t in tags_list:
                     t = t.strip()
                     tag, is_tag_created = Tag.objects.get_or_create(name=t)
-                    if is_tag_created :
+                    if is_tag_created:
                         tag.slug = slugify(t, allow_unicode=True)
                         tag.save()
                     self.object.tags.add(tag)
             return response
-        else :
+        else:
             return redirect('/blog/')
 
-def category_page(request, slug) :
-    if slug == 'no_category' :
+
+def category_page(request, slug):
+    if slug == 'no_category':
         category = '미분류'
         post_list = Post.objects.filter(category=None)
-    else :
+    else:
         category = Category.objects.get(slug=slug)
         post_list = Post.objects.filter(category=category)
 
@@ -83,7 +88,7 @@ def category_page(request, slug) :
     )
 
 
-def tag_page(request, slug) :
+def tag_page(request, slug):
     tag = Tag.objects.get(slug=slug)
     post_list = tag.post_set.all()
 
@@ -98,17 +103,50 @@ def tag_page(request, slug) :
         }
     )
 
-class PostUpdate(LoginRequiredMixin, UpdateView) :
+
+class PostUpdate(LoginRequiredMixin, UpdateView):
     model = Post
-    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
+    fields = ['title', 'hook_text', 'content',
+        'head_image', 'file_upload', 'category']
 
     template_name = 'blog/post_update_form.html'
 
-    def dispatch(self, request, *args, **kwargs) :
-        if request.user.is_authenticated and request.user == self.get_object().author :
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user == self.get_object().author:
             return super(PostUpdate, self).dispatch(request, *args, **kwargs)
-        else :
+        else:
             raise PermissionDenied
+
+    def get_context_data(self, **kwargs):
+        context = super(PostUpdate, self).get_context_data()
+        if self.object.tags.exists():
+            tags_str_list = list()
+            for t in self.object.tags.all():
+                tags_str_list.append(t.name)
+            context['tags_str_default'] = '; '.join(tags_str_list)
+        return context
+
+    def form_valid(self, form) :
+        response = super(PostUpdate, self).form_valid(form)
+        self.object.tags.clear()
+
+        tags_str = self.request.POST.get('tags_str')
+
+        if tags_str :
+            tags_str = tags_str.strip()
+            tags_str = tags_str.replace(',', ';')
+            tags_list = tags_str.split(';')
+
+            for t in tags_list:
+                t = t.strip()
+                tag, is_tag_created = Tag.objects.get_or_create(name=t)
+                if is_tag_created:
+                    tag.slug = slugify(t,  allow_unicode=True)
+                    tag.save()
+                self.object.tags.add(tag)
+
+        return response
+
 
 
 # FBV 방식으로 구현
